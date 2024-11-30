907ef0777b21c954148ccbd1a07a6b30fdc4a860
Arafat <arafat.hossain1184@gmail.com>
2024-12-01 00:53:09 +0600

    first commit

--- Queue2.c
+++ Queue2.c
@@ -1,3 +1,14 @@
 /*
-** This is Double Ended Queueu(Deque).
+** This is Double Ended Queueu(Deque). We can insert elements from the rear end or from the front ends.
 */
+
+#include<stdio.h>
+#include<stdbool.h>
+
+#define size 10
+
+int Queue[size];
+int rear = -1;
+int front = -1;
+
+
